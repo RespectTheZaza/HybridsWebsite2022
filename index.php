5d6f191f39faf46c8327b7d9bb350441ae080c28
Mohamed Darawsha <81744675+RespectTheZaza@users.noreply.github.com>
2022-03-30 23:02:49 -0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="Hybrid" content="width=device-widthm initial-scale=1.0">
+    <title>Info Hybrid</title>
+    <link rel="stylesheet" href="html/main.css">
+  </head>
+  <body>
+    <div class="navbar">
+        <div class="container">
+          <img src="images/logo2.png">
+
+          <img id="mobile-cta" class="moblie-menu" src="images/menu.svg" alt="Open Navagation">
+
+          <nav>
+              <img id="mobile-cta" class="mobile-menu-exit" src="images/exit.svg">
+
+              <ul class="primary-nav">
+                  <li class="current"><a href="#">Home</a></li>
+                  <li><a href="#">About me</a></li>
+                  <li><a href="#">Socials</a></li>
+              </ul>
+
+              <ul class="secondary-nav">
+                <li><a href="#">Contact Me</a></li>
+                <li class="org-cta"><a href="#">Team/Organization</a></li>
+            </ul>
+          </nav>
+        </div>
+    </div>
+
+    <section class="hero">
+      <div class="container">
+          <div class="left.col">
+              <p class="subhead">Watch Live</p>
+              <h1>WATCH ME LIVE!</h1>
+
+              <div class="hero-cta">
+                <a href="#" class="primary-cta">Watch on Twitch</a>
+                <a href="sub-cta" class="subscribe-cta">
+                    <img src="images/twitch.svg" alt="Subscribe To Support My Channel!">Subscribe To Support My Channel
+                </a>
+              </div>
+          </div>
+
+          <img src="images/orgban.png" class="hero-img" alt="Illustration">
+        </div>
+    </section>
+
+
+  </body>
+</html>
